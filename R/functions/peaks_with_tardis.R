b9bf40346394c3dedd5bf83309931c42e0650478
pablovgd <pablovgd@gmail.com>
2024-02-02 15:13:03 +0100

    Implemented retention time correction based on internal standards in QC's.

--- R/functions/peaks_with_tardis.R
+++ R/functions/peaks_with_tardis.R
@@ -11,7 +11,8 @@ tardis_peaks <-
            batch_mode,
            batch_positions,
            QC_pattern,
-           sample_pattern) {
+           sample_pattern,
+           int_std_id) {
     #Create empty dataframe for sample results
     results_samples <-
       data.frame(
@@ -49,11 +50,12 @@ tardis_peaks <-
     #save compound info
     info_compounds <- dbData
     
-    
+    #Loop over the batches
     
     for (batchnr in 1:length(batch_positions)) {
       dbData <- info_compounds
       
+      #Empty dataframe for results of the QCs
       results_QCs_batch <-
         data.frame(
           Component = character(0),
@@ -64,36 +66,131 @@ tardis_peaks <-
           foundRT = numeric(0)
         )
       
+      #Select files in the batch
       files_batch <-
         files[batch_positions[[batchnr]][1]:batch_positions[[batchnr]][2]]
       
+      #Subselection, only the sample files
       sample_files <-
         files_batch[grep(pattern = QC_pattern, files_batch, invert = TRUE)]
+      #Subselection, only the sample files 
       QC_files <-
         files_batch[grep(pattern = QC_pattern, files_batch)]
       
-      #First do the whole analysis for the QC's to find the RT time where the peaks are found.
+      #Load all the data for all files in the batch
       
       data_batch <- readMsExperiment(spectraFiles = files_batch,
                                      backend = MsBackendMzR(),
                                      BPPARAM = SnowParam(workers = 1)
       )
       
-
+      #Define study and QC samples
       sampleData(data_batch)$sample_type <- "study"
       sampleData(data_batch)$sample_type[grep(pattern = QC_pattern, files_batch)] <- "QC"
       
+      #Subselect QC files, first we will locate the peaks of the internal standards in the QC files of this batch to align the rest of the batch data to
       
-      rtmed <- dbData$tr
-      #param <- ObiwarpParam(binSize = 0.1,subset = which(sampleData(data_batch)$sample_type == "QC"))
-      rts <- as.matrix(cbind(rtmed,rtmed,rtmed,rtmed))
-      internal_standards <- rts[c(19,32,30,33,31),]
-      param <- PeakGroupsParam(minFraction = 0.5, peakGroupsMatrix = internal_standards, subset = which(sampleData(data_batch)$sample_type == "QC"))
+      data_QC <- data_batch[which(sampleData(data_batch)$sample_type == "QC")]
+      #Extract spectra
+      spectra_QC <- data_QC@spectra
+      #Create ranges for all compounds
+      ranges <- createRanges(data_QC, dbData, ppm, rtdev)
+      #Get mz & rt ranges
+      mzRanges <- ranges[[1]]
+      rtRanges <- ranges[[2]]
+      
+      #Get the ranges for the internal standard compounds
+      
+      internal_standards_rt <- rtRanges[which(dbData$ID %in% int_std_id),]
+      internal_standards_mz <- mzRanges[which(dbData$ID %in% int_std_id),]
+      
+      #Get QC sample names
+      sample_names <-
+        lapply(data_QC@sampleData$spectraOrigin, basename)
+      
+      #Initiate empty vectors
+      int_std_foundrt <- c()
+      int_std <- c()
+      
+      #Retrieve foundRT of internal standards in QC's
+      for (j in 1:dim(internal_standards_rt)[1]) {
+        rt_list = list()
+        int_list = list()
+        x_list = list()
+        y_list = list()
+        
+        
+        
+        for (i in 1:length(sample_names)) {
+          sample_name <- unlist(sample_names[i])
+          
+          #extraction of rt & int with chromatograms (slow)
+          # chromatograms <-
+          #   chromatogram(data_QC, rt = rtRanges[j, ], mz = mzRanges[j, ],aggregationFun = "max")
+          # rt <- chromatograms@.Data[[i]]@rtime
+          # int <- chromatograms@.Data[[i]]@intensity
+          
+          #extraction of rt & int using spectra (fast, but diff results??)
+          
+          
+          .sum_intensities <- function(x, ...) {
+            if (nrow(x)) {
+              cbind(mz = NA_real_,
+                    intensity = sum(x[, "intensity"], na.rm = TRUE))
+            } else
+              cbind(mz = NA_real_, intensity = NA_real_)
+          }
+          
+          sample_spectra <-
+            filterDataOrigin(spectra_QC, unique(dataOrigin(spectra_QC))[i])
+          sample_spectra <-
+            filterRt(sample_spectra, internal_standards_rt[j,])
+          sample_spectra <-
+            filterMzRange(sample_spectra, internal_standards_mz[j,])
+          
+          sfs_agg <-
+            addProcessing(sample_spectra, .sum_intensities)
+          eic <-
+            cbind(rtime(sfs_agg),
+                  unlist(intensity(sfs_agg), use.names = FALSE))
+          
+          
+          
+          
+          rt <- eic[, 1]
+          int <- eic[, 2]
+          
+          int[which(is.na(int))] = 0
+          
+          smoothed <- sgolayfilt(int, p = 3, n = 7)
+          
+          border <- find_peak_points(rt, smoothed,dbData$tr[j])
+          
+          int_std_foundrt <- cbind(int_std_foundrt, border[[3]])
+          
+        }
+        
+        int_std <- rbind(int_std,int_std_foundrt)
+        int_std_foundrt <- c()
+      }
       
 
-      data_batch <- adjustRtime(data_batch, param = param)
+      # rtmed <- dbData$tr
+      # param <- ObiwarpParam(binSize = 0.1,subset = which(sampleData(data_batch)$sample_type == "QC"))
+      # rts <- as.matrix(cbind(rtmed,rtmed,rtmed,rtmed))
+      # internal_standards <- rts[c(19,32,30,33,31),]
       
+      #Define parameters for retention time adjustment, based on the QC's and the internal standards
+      param <- PeakGroupsParam(minFraction = 0.9, peakGroupsMatrix = int_std, subset = which(sampleData(data_batch)$sample_type == "QC"))
+      
+      #Adjust the RT
+      data_batch <- adjustRtime(data_batch, param = param)
+      #Apply the adjusted RT 
       data_batch <- applyAdjustedRtime(data_batch)
+      
+      
+      #Now, we try and find ALL compounds in the QC samples and save their foundRT to search the compounds at that RT in the sample files
+      
       
       data_QC <- data_batch[which(sampleData(data_batch)$sample_type == "QC")]
       
@@ -115,9 +212,7 @@ tardis_peaks <-
       sample_names <-
         lapply(data_QC@sampleData$spectraOrigin, basename)
       
-      ranges <- createRanges(data_batch, dbData, ppm, rtdev)
-      mzRanges <- ranges[[1]]
-      rtRanges <- ranges[[2]]
+    
       
       spectra <- data_QC@spectra
       # 
@@ -265,7 +360,8 @@ tardis_peaks <-
       }
       
       results_QCs <- rbind(results_QCs, results_QCs_batch)
-      #Next do the whole analysis for the samples in the same batch of the QC's to find the compounds at the corrected RT.
+      
+      #Next do the whole analysis for the samples in the same batch of the QC's to find ALL the compounds at the corrected RT.
       
       
       # data_samples <-
@@ -275,10 +371,12 @@ tardis_peaks <-
       #     BPPARAM = SnowParam(workers = 1)
       #   )
       
+      #Get sample data
       data_samples <- data_batch[which(sampleData(data_batch)$sample_type == "study")]
       
       
-      # data_samples <- adjustRtime(data_samples,param = ObiwarpParam())
+      # data_samples <- adjustRtime(data_samples,param = ObiwarpParam()) 
+    
       
       sample_names <-
         lapply(data_samples@sampleData$spectraOrigin, basename)
@@ -303,6 +401,7 @@ tardis_peaks <-
         }
       }
       
+      #Create ranges around new RT 
       ranges <- createRanges(data_samples, dbData, ppm, rtdev)
       mzRanges <- ranges[[1]]
       rtRanges <- ranges[[2]]
