bb1f4a51119057362ef12dff305b1f96d6b26c0e
pablovgd <pablovgd@gmail.com>
2024-12-16 13:22:52 +0100

    fix typo in extractPeak

--- R/extractPeak.R
+++ R/extractPeak.R
@@ -35,7 +35,7 @@ filterSpectra <- function(spectra,dataOrigin,rt_range,mz_range) {
 #' @export
 extract_eic <- function(spectra){
   sfs_agg <-
-    addProcessing(sample_spectra, .sum_intensities)
+    addProcessing(spectra, .sum_intensities)
   eic <-
     cbind(rtime(sfs_agg),
           unlist(intensity(sfs_agg), use.names = FALSE))
